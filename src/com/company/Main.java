e5735122b621c0177ed6b7b78d68612eacee96e2
omurbekow <arturomurbekov1@gmail.com>
2021-11-23 21:18:26 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int temperature = 25;
+        int ageOfMen = 13;
+
+        if (temperature > 20 && ageOfMen < 25)
+            System.out.println("Можно идти гулять");
+        if (temperature < 28 && ageOfMen < 20)
+            System.out.println("Можно идти гулять");
+        if (temperature < 25 && ageOfMen > 45)
+            System.out.println("Оставайтесь дома");
+
+        int temperature2 = 12;
+        int ageOfMen2 = 8;
+
+        if (temperature2 > 20 && ageOfMen2 < 25)
+            System.out.println("Можно идти гулять");
+        if (temperature2 < 28 && ageOfMen2 < 20)
+            System.out.println("Можно идти гулять");
+        if (temperature2 < 25 && ageOfMen2 > 45)
+            System.out.println("Оставайтесь дома");
+
+        int temperature3 = -4;
+        int ageOfMen3 = 20;
+
+        if (temperature3 > 20 && ageOfMen3 < 25)
+            System.out.println("Можно идти гулять");
+        if (temperature3 < 28 && ageOfMen3 < 20)
+            System.out.println("Можно идти гулять");
+        if (temperature3 < 25 && ageOfMen3 > 45)
+            System.out.println("Оставайтесь дома");
+
+        int temperature4 = 0;
+        int ageOfMen4 = 39;
+
+        if (temperature4 > 20 && ageOfMen4 < 25)
+            System.out.println("Можно идти гулять");
+        if (temperature4 < 28 && ageOfMen4 < 20)
+            System.out.println("Можно идти гулять");
+        if (temperature4 < 25 && ageOfMen4 > 45)
+            System.out.println("Оставайтесь дома");
+
+        int temperature5 = 31;
+        int ageOfMen5 = 40;
+
+        if (temperature5 > 20 && ageOfMen5 < 25)
+            System.out.println("Можно идти гулять");
+        if (temperature5 < 28 && ageOfMen5 < 20)
+            System.out.println("Можно идти гулять");
+        if (temperature5 < 25 && ageOfMen5 > 45)
+            System.out.println("Оставайтесь дома");
+        }
+    }
+
